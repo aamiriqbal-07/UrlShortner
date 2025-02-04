7707295254236d5a05b409aff45abc3b7e84fc2b
aamiriqbal-07 <aamir.iqbal@iiitranchi.ac.in>
2025-02-04 23:24:32 +0530

    fix: change config file wrt docker

--- config/config.go
+++ config/config.go
@@ -1,40 +1,50 @@
 package config
 
+import (
+	"os"
+)
+
 type Config struct {
-    Server struct {
-        Port string `yaml:"port"`
-        Host string `yaml:"host"`
-    } `yaml:"server"`
-    
-    Database struct {
-        Host     string `yaml:"host"`
-        Port     string `yaml:"port"`
-        User     string `yaml:"user"`
-        Password string `yaml:"password"`
-        Name     string `yaml:"name"`
-    } `yaml:"database"`
-    
-    ShortURL struct {
-        Length    int    `yaml:"length"`
-        BaseURL   string `yaml:"base_url"`
-    } `yaml:"short_url"`
+	Server struct {
+		Port string
+		Host string
+	}
+
+	Database struct {
+		Host     string
+		Port     string
+		User     string
+		Password string
+		Name     string
+	}
+
+	ShortURL struct {
+		Length  int
+		BaseURL string
+	}
 }
 
 func LoadConfig() (*Config, error) {
-    cfg := &Config{}
-    
-    // Default values
-    cfg.Server.Port = "8080"
-    cfg.Server.Host = "localhost"
-    
-    cfg.Database.Host = "localhost"
-    cfg.Database.Port = "3306"
-    cfg.Database.User = "root"
-    cfg.Database.Password = "12345"
-    cfg.Database.Name = "urlshortner"
-    
-    cfg.ShortURL.Length = 6
-    cfg.ShortURL.BaseURL = "http://localhost:8080"
-    
-    return cfg, nil
+	cfg := &Config{}
+
+	cfg.Server.Port = getEnv("SERVER_PORT", "8080")
+	cfg.Server.Host = getEnv("SERVER_HOST", "localhost")
+
+	cfg.Database.Host = getEnv("DB_HOST", "localhost")
+	cfg.Database.Port = getEnv("DB_PORT", "3306")
+	cfg.Database.User = getEnv("DB_USER", "root")
+	cfg.Database.Password = getEnv("DB_PASSWORD", "12345")
+	cfg.Database.Name = getEnv("DB_NAME", "urlshortner")
+
+	cfg.ShortURL.Length = 6
+	cfg.ShortURL.BaseURL = "http://localhost:" + cfg.Server.Port
+
+	return cfg, nil
+}
+
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
 }
