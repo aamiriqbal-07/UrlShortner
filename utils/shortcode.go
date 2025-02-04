e66be48b3a97549776798e04ed7fa31e37e844ba
aamiriqbal-07 <aamir.iqbal@iiitranchi.ac.in>
2025-02-04 15:17:05 +0530

    feat: add utils

---
+++ utils/shortcode.go
@@ -0,0 +1,19 @@
+// utils/shortcode.go
+package utils
+
+import (
+	"math/rand"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+// Global random number generator
+var rng = rand.New(rand.NewSource(rand.Int63()))
+
+func GenerateShortCode(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[rng.Intn(len(charset))]
+	}
+	return string(b)
+}
